3d995c8a3e28dd844132ba6d49d57f313383ec57
devil_cxb <45705649@qq.com>
2018-03-22 23:53:18 +0800

    finish quickSort demo

--- practise/test.py
+++ practise/test.py
@@ -1,12 +1,3 @@
-def groups(lists, group):
-    new_lists = []
-    for i in range(group):
-        temp_list = [lists[i]]
-        j = i + group
-        while j < len(lists):
-            temp_list.append(lists[j])
-            j += group
-        new_lists.append(temp_list)
-    return new_lists
-
-print groups([7, 3, 1, 2, 4, 6, 5], 3)
+a = [7, 6]
+a[0], a[1] = a[1], a[0]
+print a
