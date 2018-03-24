7183a6e6a7feca48882ba23d7293c99faa303650
devil_cxb <45705649@qq.com>
2018-03-24 21:29:03 +0800

    add shift sort

--- practise/Sort/shift.py
+++ practise/Sort/shift.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def shift(lists, shift_num):
     """
 
@@ -5,8 +6,8 @@ def shift(lists, shift_num):
     :param shift_num:
     :return:
     """
-    lists = map(lambda x: x[1], sorted(map(lambda x: ((x + shift_num)%len(lists), lists[x]), range(len(lists)))))
-    return lists
+    return map(lambda x: x[1], sorted(map(lambda x: ((x + shift_num) % len(lists), lists[x]), range(len(lists)))))
+
 
 if __name__ == '__main__':
     print shift(range(5), 2)
