c81a6810811890a9581a3ad2f46f1ee87f645e29
devil_cxb <45705649@qq.com>
2018-03-24 16:33:13 +0800

    add functional programming

--- practise/Sort/test.py
+++ practise/Sort/test.py
@@ -1,2 +1,10 @@
-for i in range(1, 6):
-    print i
+def shift(lists, shift_num):
+    temp_lists = [None]*len(lists)
+    lists = ["a", "b", "c", "d", "e"]
+    lists = map(lambda x: (lists[x], (x + shift_num)%len(lists)), range(len(lists)))
+    for i in lists:
+        temp_lists[i[1]] = i[0]
+    return temp_lists
+
+if __name__ == '__main__':
+    print shift(["a", "b", "c", "d", "e"], -5)
